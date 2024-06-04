406f2b0914df3b06f9aa7a09d21b37ebe83cef43
Maria Salfer <maria_kalemanov@yahoo.de>
2024-06-04 15:02:22 +0000

    Add comments, fix model path

--- duration_prediction_serve/serve.py
+++ duration_prediction_serve/serve.py
@@ -2,14 +2,14 @@ import pickle
 import os
 from flask import Flask, request, jsonify
 
-default_model_file = 'model.bin'
+default_model_file = '../models/model-2022-01.bin'
 MODEL_PATH = os.getenv('MODEL_PATH', default_model_file)
 
 with open(MODEL_PATH, 'rb') as f_in:
     model = pickle.load(f_in)
 
 # "feature engineering"
-def prepare_features(ride):
+def prepare_features(ride): # just turns categorical features from ineger into string
     features = {}
     features['PULocationID'] = str(ride['PULocationID'])
     features['DOLocationID'] = str(ride['DOLocationID'])
@@ -27,7 +27,7 @@ def predict(features):
 
 @app.route('/predict', methods=['POST'])
 def predict_endpoint():
-    trip = request.get_json()
+    trip = request.get_json() # because get in json object
 
     features = prepare_features(trip)
     pred = predict(features)
@@ -43,7 +43,7 @@ def predict_endpoint():
         'version': VERSION
     }
 
-    return jsonify(result)
+    return jsonify(result) # return json object
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9696)
--- ping.py
+++ ping.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask('ping')
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return 'PONG'
+@app.route('/ping', methods=['GET']) # flask decorator: when send a GET request to the address '/ping', execute the python function ("endpoint")
+def ping(): # usual python function
+    return 'PONG' # just reply with 'PONG'
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=9696)
